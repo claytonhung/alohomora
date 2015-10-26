dd4cece3b3e6b0aabcd8cede4a7ec50de3f6ef62
Clayton Hung <hung.clayton@gmail.com>
2015-10-26 17:48:49 -0400

    push message to receiver

--- Hardware/Receiver/Receiver.ino
+++ Hardware/Receiver/Receiver.ino
@@ -1,34 +1,89 @@
- /* 
-  RF Blink - Receiver sketch 
-     Written by ScottC 17 Jun 2014
-     Arduino IDE version 1.0.5
-     Website: http://arduinobasics.blogspot.com
-     Receiver: XY-MK-5V
-     Description: A simple sketch used to test RF transmission/receiver.          
- ------------------------------------------------------------- */
+#include <VirtualWire.h> 
 
- #define rfReceivePin A0  //RF Receiver pin = Analog pin 0
- #define ledPin 13        //Onboard LED = digital pin 13
+// Pins definition
+const int led_pin = 13;
+const int receive_pin = 12;
+int pinSpeaker = 10; 
 
- unsigned int data = 0;   // variable used to store received data
- const unsigned int upperThreshold = 70;  //upper threshold value
- const unsigned int lowerThreshold = 50;  //lower threshold value
+void setup(){
+   Serial.begin(9600); // Debugging only
+   // Initialise the IO and ISR
+   vw_set_rx_pin(receive_pin);
+   vw_setup(4000); // Transmission rate
+   // Start the receiver PLL
+   vw_rx_start();
+   // Set LED pin and Buzzer
+   pinMode(led_pin, OUTPUT);
+   pinMode(pinSpeaker, OUTPUT);
+}
 
- void setup(){
-   pinMode(ledPin, OUTPUT);
-   Serial.begin(9600);
- }
+ 
 
- void loop(){
-   data=analogRead(rfReceivePin);    //listen for data on Analog pin 0
-   
-    if(data>upperThreshold){
-     digitalWrite(ledPin, LOW);   //If a LOW signal is received, turn LED OFF
-     Serial.println(data);
+void loop(){
+   uint8_t buf[VW_MAX_MESSAGE_LEN];
+   uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+   // Check if a message was received
+    if (vw_get_message(buf, &buflen)) {
+      if(buf[0]=='1'){
+        Serial.println("Motion detected!");
+        digitalWrite(led_pin,1);
+        playTone(300, 160);
+        delay(150);
+      }  
+
+     if(buf[0]=='0'){
+       Serial.println("Motion ended!");
+       digitalWrite(led_pin,0);
+       playTone(0, 0);
+       delay(300); 
+     }
    }
-   
-   if(data<lowerThreshold){
-     digitalWrite(ledPin, HIGH);   //If a HIGH signal is received, turn LED ON
-     Serial.println(data);
+}
+
+
+
+// duration in mSecs, frequency in hertz
+
+void playTone(long duration, int freq) {
+    duration *= 1000;
+    int period = (1.0 / freq) * 1000000;
+    long elapsed_time = 0;
+    
+    while (elapsed_time < duration) {
+      digitalWrite(pinSpeaker,HIGH);
+      delayMicroseconds(period / 2);
+      digitalWrite(pinSpeaker, LOW);
+      delayMicroseconds(period / 2);
+    
+      elapsed_time += (period);
    }
- }
+}
+
+
+//
+// #define rfReceivePin A0  //RF Receiver pin = Analog pin 0
+// #define ledPin 13        //Onboard LED = digital pin 13
+//
+// unsigned int data = 0;   // variable used to store received data
+// const unsigned int upperThreshold = 70;  //upper threshold value
+// const unsigned int lowerThreshold = 50;  //lower threshold value
+//
+// void setup(){
+//   pinMode(ledPin, OUTPUT);
+//   Serial.begin(9600);
+// }
+//
+// void loop(){
+//   data=analogRead(rfReceivePin);    //listen for data on Analog pin 0
+//   
+//    if(data>upperThreshold){
+//     digitalWrite(ledPin, LOW);   //If a LOW signal is received, turn LED OFF
+//     Serial.println(data);
+//   }
+//   
+//   if(data<lowerThreshold){
+//     digitalWrite(ledPin, HIGH);   //If a HIGH signal is received, turn LED ON
+//     Serial.println(data);
+//   }
+// }

8fd30b22e10ee550e70549da657cddc7d3835e95
Clayton Hung <hung.clayton@gmail.com>
2015-11-09 21:52:11 -0500

    added incoming_byte

--- Hardware/Transmitter/Transmitter.ino
+++ Hardware/Transmitter/Transmitter.ino
@@ -7,29 +7,36 @@ const int transmit_pin = 12;
 const int receive_pin = 2;
 const int transmit_en_pin = 3;
 
- void setup(){
-   // Initialise the IO and ISR
+void setup(){
+  // Initialise the IO and ISR
   vw_set_tx_pin(transmit_pin);
   vw_set_rx_pin(receive_pin);
   vw_set_ptt_pin(transmit_en_pin);
   vw_set_ptt_inverted(true); // Required for DR3100
   vw_setup(2000);	 // Bits per sec
-   Serial.begin(9600);
- }
- 
+  Serial.begin(9600);
+}
+
 byte count = 1;
 
 void loop(){
-  char msg[7] = {'h','e','l','l','o',' ','#'};
-  msg[6] = count;
-  digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
-  vw_send((uint8_t *)msg, 7);
-        if(vx_tx_active()) {
-        Serial.print("THIS BOY SENT STUFF");
-      }
-  vw_wait_tx(); // Wait until the whole message is gone
-  digitalWrite(led_pin, LOW);
-  delay(1000);
-  count = count + 1;
-//      vw_send((uint8_t *)msg, strlen(msg));
+  
+  if (incoming_byte == 1) {
+    char msg[7] = {'h','e','l','l','o',' ','#'  };
+    msg[6] = count;
+    digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg, 7);
+    
+    if(vx_tx_active()) {
+      Serial.print("THIS BOY SENT STUFF");
+      Serial.println(msg);
+    }
+    
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(led_pin, LOW);
+    delay(1000);
+    count = count + 1;
+  }
+  //      vw_send((uint8_t *)msg, strlen(msg));
 }
+

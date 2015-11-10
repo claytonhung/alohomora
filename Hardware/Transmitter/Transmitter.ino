607885fdc232c3e88ab90000b74334288dfddf0d
Clayton Hung <hung.clayton@gmail.com>
2015-11-09 21:47:52 -0500

    send hello

--- Hardware/Transmitter/Transmitter.ino
+++ Hardware/Transmitter/Transmitter.ino
@@ -1,66 +1,35 @@
 #include <VirtualWire.h>
 
-int led_pin = 13;
-int transmit_pin = 12;
-int val = 0;
+int incoming_byte = 0; //Value from phone
+
+const int led_pin = 13;
+const int transmit_pin = 12;
+const int receive_pin = 2;
+const int transmit_en_pin = 3;
 
  void setup(){
-   Serial.begin(38400);
-   vw_set_tx_pin(transmit_pin);
-   vw_setup(4000); //Transmission rate
-   pinMode(led_pin, OUTPUT);   
+   // Initialise the IO and ISR
+  vw_set_tx_pin(transmit_pin);
+  vw_set_rx_pin(receive_pin);
+  vw_set_ptt_pin(transmit_en_pin);
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);	 // Bits per sec
+   Serial.begin(9600);
  }
-
-// void loop(){
-//   Serial.println("START");
-//   
-//   char msg[1] = {'0'};
-//   val = digitalRead(pir_pin);
-//   if (val == 1){ //Motion detected
-//     msg[0] = '1';
-//   for(int i=4000; i>5; i=i-(i/3)){
-//     analogWrite(10)
-//     //digitalWrite(rfTransmitPin, HIGH);     //Transmit a HIGH signal
-//     //digitalWrite(ledPin, HIGH);       //Turn the LED on
-//     delay(2000);                           //Wait for 1 second
-//     
-//     digitalWrite(rfTransmitPin,LOW);      //Transmit a LOW signal
-//     digitalWrite(ledPin, LOW);            //Turn the LED off
-//     delay(i);                            //Variable delay
-//   }
-// }
  
+byte count = 1;
+
 void loop(){
-  Serial.println("START");
-  char msg[1] = {'0'};
-  // Get sensor value
-//  val = digitalRead(pir_pin);
-  // Change message if motion is detected
-  if (val == 1){
-      msg[0] = '1';
-      digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
-      
-      vw_send((uint8_t *)msg, 1);
-      vw_wait_tx(); // Wait until the whole message is gone
-
-//      if (pir_state == LOW) {
-//        Serial.println("Motion detected!");
-//        pir_state = HIGH;
-//      }
-   }
-   else{
-     msg[0] = '0';
-     digitalWrite(led_pin, LOW);
-     
-     vw_send((uint8_t *)msg, 1);
-     vw_wait_tx(); // Wait until the whole message is gone
-
-//     if (pir_state == HIGH){
-//        Serial.println("Motion ended!");
-//        pir_state = LOW;
-//     }
-   }
+  char msg[7] = {'h','e','l','l','o',' ','#'};
+  msg[6] = count;
+  digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
+  vw_send((uint8_t *)msg, 7);
+        if(vx_tx_active()) {
+        Serial.print("THIS BOY SENT STUFF");
+      }
+  vw_wait_tx(); // Wait until the whole message is gone
+  digitalWrite(led_pin, LOW);
+  delay(1000);
+  count = count + 1;
+//      vw_send((uint8_t *)msg, strlen(msg));
 }
-
-
-
